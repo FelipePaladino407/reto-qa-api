7ffd02a52f6650c0ccd4ae5aea4750c5e56d98cf
Felipe <felipaladino05@gmail.com>
2025-12-13 10:29:27 -0300

    haha

--- E2E/pages/IMBd_homePage.py
+++ E2E/pages/IMBd_homePage.py
@@ -6,8 +6,8 @@ class ImdbHomePage:
         self.search_box = 'input[name="q"]'
         self.search_button = 'button[type="submit"]'
 
-def search_movie(self, movie_name: str):
-    self.page.fill(self.search_box, movie_name)
-    self.page.click(self.search_button)
-    self.page.wait_for_navigation()
+    def search_movie(self, movie_name: str):
+        self.page.fill(self.search_box, movie_name)
+        self.page.click(self.search_button)
+        self.page.wait_for_navigation()
     
